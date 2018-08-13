56debae98b6d6cffb1fa94a432c6576437fa7a8b
Vik <vik@ktwzk.me>
2018-08-13 19:31:44 +0500

    Много обновлений: 1. Добавил requirements 2. Дописал инструкцию 3. Добавил возможность указать свой секрет 4. Обновил .gitignore, чтобы он не добавлял сессию telethon и __pycache__ в git 5. Чуть-чуть улучшил производительность 6. PEP8

--- README.md
+++ README.md
@@ -1,18 +1,23 @@
 # OpenTFD
 * Merges serial Telegram messages, if between them a pause of less than 30 seconds
-* Integrated google translator
+* Integrated Google translator
 ### Compiled
 * opentfd-win-noproxy.exe - windows daemon without proxy
 * opentfd-win-proxy.exe - windows daemon with proxy
 * opentfd-linux-noproxy - linux daemon without proxy
 * opentfd-ubuntu-noproxy - ubunti daemon without proxy
+### Scripts
 * opentfd.service - template of linux systemd service for 24/7 execution on VDS
+* Dockerfile
 
-requires latest version of Telethon
-http://telethon.readthedocs.io/en/stable/
+### For non-compiled versions
+1. `pip3 install reqirements.txt`
+2. Add (API token and hash)[https://core.telegram.org/api/obtaining_api_id] to secret.template.py and rename it to secret.py
+3. `python3 opentfd.py`
 
-Translator:
-https://github.com/mouuff/mtranslate.git
+### Requirements
+* Latest version of Telethon: http://telethon.readthedocs.io/en/stable/
+* Translator: https://github.com/mouuff/mtranslate.git
 
 Telegram канал разработчика 
 https://t.me/MediaTube_stream
--- opentfd.py
+++ opentfd.py
@@ -1,52 +1,61 @@
 from datetime import timedelta
 
 import mtranslate
-from telethon import TelegramClient
-from telethon import events
+from telethon import TelegramClient, events
 
 import secret
 
-supported_langs = {'Afrikaans': 'af', 'Irish': 'ga', 'Albanian': 'sq', 'Italian': 'it', 'Arabic': 'ar',
-                   'Japanese': 'ja', 'Azerbaijani': 'az', 'Kannada': 'kn', 'Basque': 'eu', 'Korean': 'ko',
-                   'Bengali': 'bn', 'Latin': 'la', 'Belarusian': 'be', 'Latvian': 'lv', 'Bulgarian': 'bg',
-                   'Lithuanian': 'lt', 'Catalan': 'ca', 'Macedonian': 'mk', 'Chinese Simplified': 'zh-CN',
-                   'Malay': 'ms', 'Chinese Traditional': 'zh-TW', 'Maltese': 'mt', 'Croatian': 'hr', 'Norwegian': 'no',
-                   'Czech': 'cs', 'Persian': 'fa', 'Danish': 'da', 'Polish': 'pl', 'Dutch': 'nl', 'Portuguese': 'pt',
-                   'English': 'en', 'Romanian': 'ro', 'Esperanto': 'eo', 'Russian': 'ru', 'Estonian': 'et',
-                   'Serbian': 'sr', 'Filipino': 'tl', 'Slovak': 'sk', 'Finnish': 'fi', 'Slovenian': 'sl',
-                   'French': 'fr', 'Spanish': 'es', 'Galician': 'gl', 'Swahili': 'sw', 'Georgian': 'ka',
-                   'Swedish': 'sv', 'German': 'de', 'Tamil': 'ta', 'Greek': 'el', 'Telugu': 'te', 'Gujarati': 'gu',
-                   'Thai': 'th', 'Haitian Creole': 'ht', 'Turkish': 'tr', 'Hebrew': 'iw', 'Ukrainian': 'uk',
-                   'Hindi': 'hi', 'Urdu': 'ur', 'Hungarian': 'hu', 'Vietnamese': 'vi', 'Icelandic': 'is',
+supported_langs = {'Afrikaans': 'af', 'Irish': 'ga', 'Albanian': 'sq',
+                   'Italian': 'it', 'Arabic': 'ar', 'Japanese': 'ja',
+                   'Azerbaijani': 'az', 'Kannada': 'kn', 'Basque': 'eu',
+                   'Korean': 'ko', 'Bengali': 'bn', 'Latin': 'la',
+                   'Belarusian': 'be', 'Latvian': 'lv', 'Bulgarian': 'bg',
+                   'Lithuanian': 'lt', 'Catalan': 'ca', 'Macedonian': 'mk',
+                   'Chinese Simplified': 'zh-CN', 'Malay': 'ms',
+                   'Chinese Traditional': 'zh-TW', 'Maltese': 'mt',
+                   'Croatian': 'hr', 'Norwegian': 'no', 'Czech': 'cs',
+                   'Persian': 'fa', 'Danish': 'da', 'Polish': 'pl',
+                   'Dutch': 'nl', 'Portuguese': 'pt', 'English': 'en',
+                   'Romanian': 'ro', 'Esperanto': 'eo', 'Russian': 'ru',
+                   'Estonian': 'et', 'Serbian': 'sr', 'Filipino': 'tl',
+                   'Slovak': 'sk', 'Finnish': 'fi', 'Slovenian': 'sl',
+                   'French': 'fr', 'Spanish': 'es', 'Galician': 'gl',
+                   'Swahili': 'sw', 'Georgian': 'ka', 'Swedish': 'sv',
+                   'German': 'de', 'Tamil': 'ta', 'Greek': 'el',
+                   'Telugu': 'te', 'Gujarati': 'gu', 'Thai': 'th',
+                   'Haitian Creole': 'ht', 'Turkish': 'tr', 'Hebrew': 'iw',
+                   'Ukrainian': 'uk', 'Hindi': 'hi', 'Urdu': 'ur',
+                   'Hungarian': 'hu', 'Vietnamese': 'vi', 'Icelandic': 'is',
                    'Welsh': 'cy', 'Indonesian': 'id', 'Yiddish': 'yi'}
-client = TelegramClient('opentfd_session', secret.api_id, secret.api_hash).start()
+client = TelegramClient(
+    'opentfd_session',
+    secret.api_id,
+    secret.api_hash
+).start()
 last_msg = None
 break_date = None
-translated = mtranslate.translate('Тест', 'en', 'auto')
-print(translated)
 
 
 @client.on(events.Raw())
-async def typing_handler(event):
-    drafts = await  client.get_drafts()
+async def translator(event):
+    drafts = await client.get_drafts()
     for draft in drafts:
         if draft.is_empty:
             continue
         text = str(draft.text)
-        for lang_code in supported_langs.values():
-            if text.endswith('/{0}'.format(lang_code)):
-                translated = mtranslate.translate(text[:-(len(lang_code)+1)], lang_code, 'auto')
-                await draft.set_message(text=translated)
-        print(draft.text)
+        if text[-3] == '/' and text[-2:] in list(supported_langs.values()):
+            translated = mtranslate.translate(text[:-2], lang_code, 'auto')
+            await draft.set_message(text=translated)
 
 
 @client.on(events.NewMessage(incoming=True))
-async def inc_handler(event):
+async def break_updater(event):
     global break_date
     global last_msg
     if last_msg:
         try:
-            if event.message.to_id.user_id == last_msg.from_id and last_msg.to_id.user_id == event.message.sender_id:
+            if (event.message.to_id.user_id == last_msg.from_id and
+                    last_msg.to_id.user_id == event.message.sender_id):
                 break_date = event.date
         except Exception:
             if event.to_id == last_msg.to_id:
@@ -54,11 +63,12 @@ async def inc_handler(event):
 
 
 @client.on(events.NewMessage(outgoing=True))
-async def out_handler(event):
+async def merger(event):
     global last_msg
     global break_date
     try:
-        if event.media or event.fwd_from or event.via_bot_id or event.reply_to_msg_id or event.reply_markup:
+        if (event.media or event.fwd_from or event.via_bot_id or
+                event.reply_to_msg_id or event.reply_markup):
             last_msg = None
         elif last_msg is None:
             last_msg = event
@@ -68,7 +78,8 @@ async def out_handler(event):
                     last_msg = event
                     break_date = 0
             elif event.date - last_msg.date < timedelta(seconds=30):
-                last_msg = await last_msg.edit('{0}\n{1}'.format(last_msg.text, event.text))
+                last_msg = await last_msg.edit(
+                    '{0}\n{1}'.format(last_msg.text, event.text))
                 last_msg.date = event.date
                 await event.delete()
             else:
